7b99acb1d1eb9f4bdc3d48bd76fd5e485b10cafc
Steffin Rayen <stefinrayen@gmail.com>
2016-12-26 15:08:33 +0530

    Create S2Q15.py
    Display even numbers between two intervals.

---
+++ Beginner/Python/S2Q15.py
@@ -0,0 +1,10 @@
+num = raw_input().split()
+num1,num2 = num
+num1 = int(num1)
+num2 = int(num2)
+if (num1%2 != 0):
+  for _ in range(num1+1,num2+1,2):
+    print(_)
+else:
+  for _ in range(num1,num2+1,2):
+    print(_)
