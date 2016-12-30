c6292c6b84413688cfd2ed05bb3191efdae39c77
Steffin Rayen <stefinrayen@gmail.com>
2016-12-30 07:55:50 +0530

    Create S1Q09.py
    Count prime numbers in a given range

---
+++ Player/Python/S1Q09.py
@@ -0,0 +1,13 @@
+lower,upper = raw_input().split()
+lower = int(lower)
+upper = int(upper)
+
+count = 0
+for num in range(lower,upper + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           count +=1
+print(count)
